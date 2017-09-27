2e182a6bbc3ffe0d5d599a8c7a7500ddf3d25761
Blake Merryman <blake@blackpixel.com>
2017-09-27 17:07:00 -0500

    Search view controller

---
+++ iPhoneX/SearchTableViewController.swift
@@ -0,0 +1,28 @@
+//
+//  SearchTableViewController.swift
+//  iPhoneX
+//
+//  Created by Blake Merryman on 9/27/17.
+//  Copyright © 2017 Blake Merryman. All rights reserved.
+//
+
+import UIKit
+
+class SearchTableViewController: TableViewController {
+
+    lazy var searchController: UISearchController = {
+        let controller = UISearchController(searchResultsController: nil)
+        // really great search controller setup logic 🙃
+        return controller
+    }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureSearchController()
+    }
+
+    func configureSearchController() {
+        tableView.tableHeaderView = searchController.searchBar
+    }
+
+}
